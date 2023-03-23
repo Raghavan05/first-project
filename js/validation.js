dbcf5a7f3af430b667daa3627ef216caa7f87ba4
Ragavan01 <ragavanr08n2002@gmail.com>
2023-03-23 22:45:01 +0530

    my first project

---
+++ js/validation.js
@@ -0,0 +1,5 @@
+const loginbtn = document.getElementById("inputEmail4"),
+email = document.getElementById("inputEmail4"),
+password = document.getElementById("inputPassword4"),
+zip = document.getElementById("inputEmail4"),
+
